90fdac82a7d3dc540593940ed6aab09268e442d9
LanaVoit <lanavoit@gmail.com>
2017-04-25 16:47:58 +0400

    test

--- src/test/java/about/Tabs.java
+++ src/test/java/about/Tabs.java
@@ -37,7 +37,7 @@ public class Tabs extends turnkeye.pages.TestBase {
     assertEquals("ASTRIO (astrio.net) is the full service Internet solution agency and custom development company with strong team of IT eCommerce professionals.", driver.findElement(By.cssSelector("h4")).getText());
     assertEquals("Meet the Team", driver.findElement(By.cssSelector("h1")).getText());
     actions.moveToElement(driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/ul/li[1]"))).build().perform();
-    assertEquals("Sergey DvoskinCEO & Ño-founder", driver.findElement(By.cssSelector("#content > div.std > ul > li:nth-child(1) > div > div > div > div > h4")).getText());
+    assertEquals("Sergey Dvoskin\nCEO & Ño-founder", driver.findElement(By.cssSelector("#content > div.std > ul > li:nth-child(1) > div > div > div > div > h4")).getText());
     
     driver.findElement(By.cssSelector("a[href=\"https://astrio.net/about/testimonials.html\"]")).click();
     TimeUnit.SECONDS.sleep(5);
