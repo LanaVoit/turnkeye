de5e6b6ef4f4f0493f1503ce498ccebe5c332614
Lana Voit <lanavoit@gmail.com>
2017-06-02 10:24:19 +0400

    tab

--- src/test/java/about/Tabs.java
+++ src/test/java/about/Tabs.java
@@ -36,7 +36,7 @@ public class Tabs extends turnkeye.pages.TestBase {
     TimeUnit.SECONDS.sleep(5);
     assertEquals("ASTRIO (astrio.net) is the full service Internet solution agency and custom development company with strong team of IT eCommerce professionals.", driver.findElement(By.cssSelector("h4")).getText());
     assertEquals("Meet the Team", driver.findElement(By.cssSelector("h1")).getText());
-    actions.moveToElement(driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/ul/li[5]"))).build().perform();
+    actions.moveToElement(driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/ul/li[4]"))).build().perform();
     //assertEquals("Svetlana Baranova\nQA Specialist", driver.findElement(By.cssSelector("#content > div.std > ul > li:nth-child(5) > div > div > div > div > h4")).getText());
     assertEquals("Svetlana Baranova\nQA Specialist", driver.findElement(By.cssSelector("#content > div.std > ul > li:nth-child(4) > div > div > div > div > h4")).getText());
     
